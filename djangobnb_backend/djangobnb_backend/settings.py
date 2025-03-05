372c3242d1a41dd2c66cbaf6a65901489ac5db6b
heric-hh <erhh.015@gmail.com>
2025-03-05 13:25:05 -0600

    feat: (api) configure drf, cors, etc

--- djangobnb_backend/djangobnb_backend/settings.py
+++ djangobnb_backend/djangobnb_backend/settings.py
@@ -1,6 +1,7 @@
 import os
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,6 +20,46 @@ DEBUG = os.environ.get("DEBUG", "0") == "1"
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOST", "localhost").split(" ")
 
 AUTH_USER_MODEL = 'useraccount.User'
+
+SITE_ID = 1
+
+WEBSITE_URL="http://localhost:8000/"
+
+SIMPLE_JWT= {
+  "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+  "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+  "ROTATE_REFRESH_TOKENS": False,
+  "BLACKLIST_AFTER_ROTATION": False,
+  "UPDATE_LAST_LOGIN": False,
+  "SIGNING_KEY": "acomplexkey",
+  "ALGORITHM": "HS256"
+}
+
+ACCOUNT_USER_MODEL_USERNAME_FIELD=None
+ACCOUNT_EMAIL_REQUIRED=True
+ACCOUNT_USERNAME_REQUIRED=False
+ACCOUNT_AUTHENTICATION_METHOD='email'
+ACCOUNT_EMAIL_VERIFICATION=None
+
+REST_FRAMEWORK = {
+  'DEFAULT_AUTHENTICATION_CLASSES': (
+    'rest_framework_simplejwt.authentication.JWTAuthentication',
+  ),
+  'DEFAULT_PERMISSION_CLASSES': (
+    'rest_framework.permissions.IsAuthenticated',
+  )
+}
+
+CORS_ALLOWED_ORIGINS = [
+  'http://127.0.0.1:8000',
+  'http://127.0.0.1:3000',
+]
+
+REST_AUTH = {
+  "USE_JWT": True,
+  "JWT_AUTH_HTTPONLY": False,
+}
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -28,12 +69,22 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "rest_framework_simplejwt",
+    "allauth",
+    "allauth.account",
+    "dj_rest_auth",
+    "dj_rest_auth.registration",
+    "corsheaders",
     "useraccount",
+    "property",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
