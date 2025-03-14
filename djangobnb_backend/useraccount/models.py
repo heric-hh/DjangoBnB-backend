5287203993687f7b2868caac1ecd6cce5ce12a98
heric-hh <erhh.015@gmail.com>
2025-03-13 21:24:02 -0600

    feat: (api) sign-up implementation

--- djangobnb_backend/useraccount/models.py
+++ djangobnb_backend/useraccount/models.py
@@ -1,45 +1,45 @@
 import uuid
-from django.conf import settings
 from django.contrib.auth.models import AbstractUser, PermissionsMixin, UserManager
 from django.db import models
 
 class CustomUserManager(UserManager):
-  def _create_user(self, name, email, password, **extra_fields):
-    if not email:
-      raise ValueError("You have not specified a valid email address")
-    
-    email = self.normalize_email(email)
-    user = self.model(email=email, name=name, **extra_fields)
-    user.set_password(password)
-    user.save(using=self.db)
-    return user
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("You have not specified a valid email address")
 
-  def create_user(self, name=None, email=None, password=None, **extra_fields):
-    extra_fields.setdefault('is_staff', False)
-    extra_fields.setdefault('is_superuser', False)
-    return self._create_user(name, email, password, **extra_fields)
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-  def create_superuser(self, name=None, email=None, password=None, **extra_fields):
-    extra_fields.setdefault('is_staff', True)
-    extra_fields.setdefault('is_superuser', True)
-    return self._create_user(name, email, password, **extra_fields)
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
 
 class User(AbstractUser, PermissionsMixin):
-  id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-  email = models.EmailField(unique=True)
-  name = models.CharField(max_length=255, blank=True, null=True)
-  avatar = models.ImageField(upload_to='uploads/avatar')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    username = None  # Eliminamos username ya que usamos email
+    avatar = models.ImageField(upload_to="uploads/avatar", null=True, blank=True)
 
-  is_active = models.BooleanField(default=True)
-  is_superuser = models.BooleanField(default=False)
-  is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(blank=True, null=True)
 
-  date_join = models.DateTimeField(auto_now_add=True)
-  last_login = models.DateTimeField(blank=True, null=True)
+    objects = CustomUserManager()
 
-  objects = CustomUserManager()
-
-  USERNAME_FIELD='email'
-  EMAIL_FIELD='email'
-  REQUIRED_FIELDS=['name', ]
-  
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
