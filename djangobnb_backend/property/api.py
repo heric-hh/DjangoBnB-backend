79c5693493e93b6e53140c9b08d2c0476d5821f9
heric-hh <erhh.015@gmail.com>
2025-03-20 00:13:57 -0600

    feat: (api) add properties from nextjs, sending data through form

--- djangobnb_backend/property/api.py
+++ djangobnb_backend/property/api.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from .serializers import PropertiesListSerializer
 from .models import Property
+from .forms import PropertyForm
 
 @api_view(['GET'])
 @authentication_classes([])
@@ -13,3 +14,16 @@ def properties_list(request):
   return JsonResponse({
     'properties': serializer.data
   })
+
+@api_view(['POST', 'FILES']) 
+def create_property(request):
+    form = PropertyForm(request.POST, request.FILES)
+    if form.is_valid(): 
+        property = form.save(commit=False)
+        property.landlord = request.user
+        property.save()
+
+        return JsonResponse({'success': True})
+    else:
+        print('Error', form.errors, form.non_field_errors)
+        return JsonResponse({'errors': form.errors.as_json(), 'status':400})
