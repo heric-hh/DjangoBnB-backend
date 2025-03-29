8bf1657a78dfa4d16afb7fbc29bb6318ffa09a45
heric-hh <erhh.015@gmail.com>
2025-03-28 19:06:03 -0600

    feat: (api) book property

--- djangobnb_backend/property/api.py
+++ djangobnb_backend/property/api.py
@@ -1,8 +1,10 @@
 from django.http import JsonResponse
 
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from .serializers import PropertiesListSerializer, PropertiesDetailSerializer
-from .models import Property
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .serializers import PropertiesListSerializer, PropertiesDetailSerializer, ReservationsListSerializer
+from .models import Property, Reservation
 from .forms import PropertyForm
 
 @api_view(['GET'])
@@ -23,6 +25,18 @@ def properties_detail(request, pk):
     serializer = PropertiesDetailSerializer(property, many=False)
     return JsonResponse(serializer.data)
 
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def properties_reservations(request, pk):
+    property = Property.objects.get(pk=pk)
+    reservations = property.reservations.all()
+
+    serializer = ReservationsListSerializer(reservations, many=True)
+
+    return JsonResponse(serializer.data, safe=False)
+
+
 @api_view(['POST', 'FILES']) 
 def create_property(request):
     form = PropertyForm(request.POST, request.FILES)
@@ -35,3 +49,27 @@ def create_property(request):
     else:
         print('Error', form.errors, form.non_field_errors)
         return JsonResponse({'errors': form.errors.as_json(), 'status':400})
+
+@api_view(['POST'])
+def book_property(request, pk):
+    try:
+        start_date = request.POST.get('start_date', '')
+        end_date = request.POST.get('end_date', '')
+        number_of_nights = request.POST.get('number_of_nights', '')
+        total_price = request.POST.get('total_price', '')
+        guests = request.POST.get('guest', 1)
+        property = Property.objects.get(pk=pk)
+        Reservation.objects.create(
+                property = property,
+                start_date = start_date,
+                end_date = end_date,
+                number_of_nights = number_of_nights,
+                total_price = total_price,
+                guests = guests,
+                created_by = request.user
+                )
+        return JsonResponse({'success': True})
+
+    except Exception as e:
+        print('Error', e)
+        return JsonResponse({'success': False, 'error': str(e)}, status=400)
